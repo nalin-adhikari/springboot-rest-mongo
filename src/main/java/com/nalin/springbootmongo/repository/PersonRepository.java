e14e9620def5381c49e69e1a6ba40fdcb4ca4686
Nalin Adhikari <csitplay@gmail.com>
2016-09-29 01:35:19 +0545

    Annotation in PersonRepository and PersonManagerImpl added.

--- src/main/java/com/nalin/springbootmongo/repository/PersonRepository.java
+++ src/main/java/com/nalin/springbootmongo/repository/PersonRepository.java
@@ -4,6 +4,7 @@
 package com.nalin.springbootmongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nalin.springbootmongo.api.domain.Person;
 
@@ -11,6 +12,7 @@ import com.nalin.springbootmongo.api.domain.Person;
  * @author nalin
  *
  */
+@Repository
 public interface PersonRepository extends MongoRepository<Person, String>{
 
 }
--- src/main/java/com/nalin/springbootmongo/service/PersonManagerImpl.java
+++ src/main/java/com/nalin/springbootmongo/service/PersonManagerImpl.java
@@ -5,6 +5,8 @@ package com.nalin.springbootmongo.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.nalin.springbootmongo.api.PersonManager;
 import com.nalin.springbootmongo.api.domain.Person;
 
@@ -12,6 +14,7 @@ import com.nalin.springbootmongo.api.domain.Person;
  * @author nalin
  *
  */
+@Service
 public class PersonManagerImpl implements PersonManager {
 
 	/* (non-Javadoc)
