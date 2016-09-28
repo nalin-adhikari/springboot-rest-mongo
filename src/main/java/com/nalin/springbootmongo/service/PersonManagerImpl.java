0d4a2a9fbb34e8446963889664dfc6af2db40bc3
Nalin Adhikari <csitplay@gmail.com>
2016-09-29 01:23:46 +0545

    Implementation of PersonManager written.

---
+++ src/main/java/com/nalin/springbootmongo/service/PersonManagerImpl.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package com.nalin.springbootmongo.service;
+
+import java.util.List;
+
+import com.nalin.springbootmongo.api.PersonManager;
+import com.nalin.springbootmongo.api.domain.Person;
+
+/**
+ * @author nalin
+ *
+ */
+public class PersonManagerImpl implements PersonManager {
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#addNew(java.io.Serializable)
+	 */
+	@Override
+	public Person addNew(Person t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#addAll(java.util.List)
+	 */
+	@Override
+	public List<Person> addAll(List<Person> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#update(java.io.Serializable)
+	 */
+	@Override
+	public Person update(Person t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#remove(java.io.Serializable)
+	 */
+	@Override
+	public Person remove(Person t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#removeBy(java.io.Serializable)
+	 */
+	@Override
+	public void removeBy(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#find(java.io.Serializable)
+	 */
+	@Override
+	public Person find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#findAll()
+	 */
+	@Override
+	public List<Person> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#findWithPaging(int, int)
+	 */
+	@Override
+	public List<Person> findWithPaging(int page, int size) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.nalin.springbootmongo.api.CrudManager#countRecord()
+	 */
+	@Override
+	public Long countRecord() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
