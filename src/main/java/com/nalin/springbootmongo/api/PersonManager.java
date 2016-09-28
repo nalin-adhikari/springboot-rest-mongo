2a89c54bbbcd72d90ac099036637d0cbc7ebfb03
Nalin Adhikari <csitplay@gmail.com>
2016-09-29 01:17:47 +0545

    PersonManager interface is added.

--- src/main/java/com/nalin/springbootmongo/api/PersonManager.java
+++ src/main/java/com/nalin/springbootmongo/api/PersonManager.java
@@ -7,7 +7,7 @@ import com.nalin.springbootmongo.api.domain.Person;
 
 /**
  * @author nalin
- *
+ * @since September, 2016
  */
 public interface PersonManager extends CrudManager<Person, String>{
 
