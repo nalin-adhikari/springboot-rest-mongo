1d9af2d2fc7f5363d13431845a4bd4f88afbbed0
Nalin Adhikari <csitplay@gmail.com>
2016-09-29 01:17:18 +0545

    PersonController is added

---
+++ src/main/java/com/nalin/springbootmongo/controller/PersonController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.nalin.springbootmongo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author nalin
+ *
+ */
+@RestController
+@RequestMapping("/")
+public class PersonController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String home(){
+		return "Server is running !";
+	}
+	
+}
